3edc668682eb7caa69de0281348e807fec654169
Esteban-Somoza <estfersomoza@gmail.com>
2023-03-29 18:53:10 -0300

    centrado de escudos

--- dashboard/src/components/ColoresEquipo.jsx
+++ dashboard/src/components/ColoresEquipo.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { mask } from '../../public/ABADlogo.svg'
 
-export default function ColoresEquipo({ colores }) {
+export default function ColoresEquipo({ colores, width = '20px', height = '20px' }) {
     const [cols, setCols] = useState([])
     useEffect(() => {
         let colorArray = [];
@@ -10,7 +10,7 @@ export default function ColoresEquipo({ colores }) {
     }, [colores])
 
     return (
-        <div className='mask w-[20px] h-[20px]'>
+        <div className={`mask w-[${width}] h-[${height}] flex items-center`}>
             <div className='flex w-full h-full rounded-md overflow-hidden'>
                 {cols && cols.map(c => <div style={{ backgroundColor: `#${c}` }} key={c} className={`h-full w-full`}></div>)}
             </div>
