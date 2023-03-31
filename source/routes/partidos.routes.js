622092210247f2a49bdc4196f1f4e426abbdb891
httpini <nicolas.pinol@hotmail.com>
2023-03-31 15:16:22 -0300

    optimizado partidos.controller

--- source/routes/partidos.routes.js
+++ source/routes/partidos.routes.js
@@ -1,13 +1,11 @@
 const {Router}= require("express")
 const router = Router()
-const {select,porTorneo,  edit,edited, create,created,destroid, partido } = require("../controllers/partidos.controller")
+const {select,porTorneo, edit, edited, destroid, partido} = require("../controllers/partidos.controller")
 
 
 router.get("/", select)
 router.get("/all", partido)
 router.get("/torneo/:torneo_id", porTorneo)
-//router.get("/torneo/:torneo_id/create", create)
-//router.post("/torneo/:torneo_id/create", created)
 router.get("/torneo/:torneo_id/edit/:id", edit)
 router.put("/edit/:id", edited)
 router.delete("/delete/:id", destroid)
