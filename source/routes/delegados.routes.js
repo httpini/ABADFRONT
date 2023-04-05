280db2e4163ef39409fe17112aa6513ea0622d2e
httpini <nicolas.pinol@hotmail.com>
2023-04-05 12:24:59 -0300

    validaciones delegados OK

--- source/routes/delegados.routes.js
+++ source/routes/delegados.routes.js
@@ -1,12 +1,13 @@
 const {Router} = require("express")
 const router = Router()
 const {create, edit,created, edited, destroid}= require("../controllers/delegados.controller")
+const createEdit = require("../validaciones/delegadosCreateEdit")
 
 
 router.get("/", create)
-router.post('/created', created)
+router.post('/created', [createEdit], created)
 router.get('/edit/:id', edit)
-router.put('/edit/:id', edited)
+router.put('/edit/:id', [createEdit], edited)
 router.delete("/delete/:id", destroid)
 
 
