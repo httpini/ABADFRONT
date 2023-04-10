0146d5bda41be703a7232895bdd5180847c39ed5
httpini <nicolas.pinol@hotmail.com>
2023-04-10 17:26:52 -0300

    FINISH V0

--- dashboard/src/pages/club.jsx
+++ dashboard/src/pages/club.jsx
@@ -3,14 +3,14 @@ import Header from '@/components/Header'
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import TarjetaClub from '@/components/TarjetaClub'
-import {GiCheckedShield} from 'react-icons/gi'
+import {BsShieldShaded} from 'react-icons/bs'
 
 export default function Club({ clubes }) {
     return (
         <div>
             <Header />
             <section className='flex flex-wrap flex-col'>
-                <h1 className='text-center font-medium text-4xl w-full mt-5 text-oscuro0'><div className='flex justify-center items-center gap-5'><GiCheckedShield className='text-xl'/>Clubes<GiCheckedShield className='text-xl'/></div></h1>
+                <h1 className='text-center font-medium text-4xl w-full mt-5 text-oscuro0'><div className='flex justify-center items-center gap-5'><BsShieldShaded className='text-xl'/>Clubes<BsShieldShaded className='text-xl'/></div></h1>
 
                 <div className='w-full flex md:flex-1 flex-col md:grid md:grid-cols-2 break:grid-cols-3 2xl:grid-cols-4 grid-flow-row p-10 gap-5 break:gap-10 justify-center items-stretch'>
                     {
