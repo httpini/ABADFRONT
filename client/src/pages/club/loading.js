4be624dfbf8b8c804a5e4f82bf1f1cdc4aa31257
nachom48 <nachom48@gmail.com>
2024-04-06 00:44:51 -0300

    fix yaml

--- client/src/pages/club/loading.js
+++ client/src/pages/club/loading.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <p>LOAGING </p>
+    return <p>LOAGING</p>
     return <LoadingSkeleton />
   }
